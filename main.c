905cb79a3a017f2be2220e823bd68fb3984dc408
Elijah <eachala5@gmail.com>
2023-11-15 01:32:27 -0800

    0

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
  * @envp: env var
  * Return: int
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)), char *envp[])
+int main(int argc, char *argv[], char *envp[])
 {
-int i = 1;
+int i;
 env *ptr;
+(void)argc;
+(void)argv;
+i = 1;
 ptr = NULL;
 copy_env(&ptr, envp);
 while (i == 1)
