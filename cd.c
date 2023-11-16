67d3d0a607498e9eeb7419d2747d42d1987a910f
Elijah <eachala5@gmail.com>
2023-11-16 01:54:15 -0800

    2

--- cd.c
+++ cd.c
@@ -25,6 +25,8 @@ if (args[1] == NULL)
 getcwd(prev, 1024);
 home = _getenv(ptr, "HOME");
 chdir(home);
+print(home);
+print("\n");
 _setenv(ptr, "PWD", home);
 return;
 }
@@ -32,12 +34,16 @@ if ((_strlen(args[1]) == 1) && (args[1][0] == '-'))
 {
 getcwd(asp, 1024);
 chdir(prev);
+print(prev);
+print("\n");
 _setenv(ptr, "PWD", prev);
 _strcpy(prev, 1024, asp);
 return;
 }
 getcwd(prev, 1024);
 chdir(args[1]);
+print(args[1]);
+print("\n");
 _setenv(ptr, "PWD", args[1]);
 }
 }
